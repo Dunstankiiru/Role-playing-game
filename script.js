86d349f4b8f9c0dae3b5eac167d5d088a27bdf89
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-07 13:58:12 +0300

    Adds weapons variable

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [];
 
 const locations= [
     {
@@ -91,6 +92,8 @@ function buyHealth(){
         health += 10;
         goldText.innerText = gold;
         healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
     }
 
 }
