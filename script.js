5d5d491bb3318aae7a56f0373e7d1f31b9a9a5aa
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-07 14:30:42 +0300

    updates function buyWeapon

--- script.js
+++ script.js
@@ -133,8 +133,10 @@ function  buyWeapon(){
             text.innerText = "You do not have enough gold to buy a weapon.";
         }
     } else {
-        text.innerText = "You already have the most powerful weapon!";
-    }
+            text.innerText = "You already have the most powerful weapon!";
+            button2.innerText = "Sell weapon for 15 gold";
+            button2.onclick = sellWeapon;   
+    } 
     
 }
 
