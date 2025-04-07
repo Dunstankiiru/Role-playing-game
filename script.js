80f8439262246f103f80517bedfc8fca27d6f34d
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-07 17:07:23 +0300

    updates sellWeapon function

--- script.js
+++ script.js
@@ -144,6 +144,9 @@ function sellWeapon (){
     if (inventory.length >1){
         gold +=15;
         goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+
+        
     }
 }
 
